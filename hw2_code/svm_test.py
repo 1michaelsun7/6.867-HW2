86a5f9e8187685a9601e8931f5692d11c4c60145
Michael Sun <micsun@mit.edu>
2016-10-23 21:43:29 -0400

    moving images toto own folder

--- hw2_code/svm_test.py
+++ hw2_code/svm_test.py
@@ -5,57 +5,62 @@ import svm_dual as sd
 
 # def linear_kerfunc(x):
 
+C = [0.01,0.1,1,10,100]
 
-# parameters
-name = '4'
-print '======Training======'
-# load data from csv files
-train = loadtxt('data/data'+name+'_train.csv')
-# use deep copy here to make cvxopt happy
-X = train[:, 0:2].copy()
-Y = train[:, 2:3].copy()
+for num in xrange(1,5):
+	for c in C:
+		# parameters
+		name = str(num)
+		print '======Training======'
+		# load data from csv files
+		train = loadtxt('data/data'+name+'_train.csv')
+		# use deep copy here to make cvxopt happy
+		X = train[:, 0:2].copy()
+		Y = train[:, 2:3].copy()
 
-x_len = len(X[0])
-print(X[0], Y[0])
-training_dat = [[X[i], Y[i]] for i in xrange(len(Y))]
-print(len(training_dat))
+		x_len = len(X[0])
+		print(X[0], Y[0])
+		training_dat = [[X[i], Y[i]] for i in xrange(len(Y))]
+		print(len(training_dat))
 
-# Carry out training, primal and/or dual
-qp_sol = sd.generate_dual_solution(training_dat, 1)
-alphas = qp_sol[0][:]
-w = dot(transpose(multiply(alphas, Y)), X)
+		# Carry out training, primal and/or dual
+		qp_sol = sd.generate_dual_solution(training_dat, c)
+		alphas = qp_sol[0][:]
+		w = dot(transpose(multiply(alphas, Y)), X)
 
-# w = qp_sol[0][:x_len]
-# b = qp_sol[0][x_len]
-# print(w,b)
-# Define the predictSVM(x) function, which uses trained parameters
-def predictSVM(x):
-	return sign(dot(squeeze(w), x))
+		print("Geometric Margin: ", 1.0/linalg.norm(w))
 
-misclass = 0
-for p in xrange(len(X)):
-	if predictSVM(X[p]) != Y[p]:
-		misclass += 1
+		# w = qp_sol[0][:x_len]
+		# b = qp_sol[0][x_len]
+		# print(w,b)
+		# Define the predictSVM(x) function, which uses trained parameters
+		def predictSVM(x):
+			return sign(dot(squeeze(w), x))
 
-print("Training Error: ", float(misclass)/len(X))
+		misclass = 0
+		for p in xrange(len(X)):
+			if predictSVM(X[p]) != Y[p]:
+				misclass += 1
 
-# plot training results
-plotDecisionBoundary(X, Y, predictSVM, [-1, 0, 1], title = 'SVM Train')
+		print("Training Error: ", float(misclass)/len(X))
+
+		# plot training results
+		#plotDecisionBoundary(X, Y, predictSVM, [-1, 0, 1], title = 'SVM Train')
 
 
-print '======Validation======'
-# load data from csv files
-validate = loadtxt('data/data'+name+'_validate.csv')
-X = validate[:, 0:2]
-Y = validate[:, 2:3]
+		print '======Validation======'
+		# load data from csv files
+		validate = loadtxt('data/data'+name+'_validate.csv')
+		X = validate[:, 0:2]
+		Y = validate[:, 2:3]
 
-misclass = 0
-for p in xrange(len(X)):
-	if predictSVM(X[p]) != Y[p]:
-		misclass += 1
+		misclass = 0
+		for p in xrange(len(X)):
+			if predictSVM(X[p]) != Y[p]:
+				misclass += 1
 
-print("Validation Error: ", float(misclass)/len(X))
+		print("Validation Error: ", float(misclass)/len(X))
 
-# plot validation results
-plotDecisionBoundary(X, Y, predictSVM, [-1, 0, 1], title = 'SVM Validate')
-pl.show()
+		# plot validation results
+		#plotDecisionBoundary(X, Y, predictSVM, [-1, 0, 1], title = 'SVM Validate')
+		#pl.show()
