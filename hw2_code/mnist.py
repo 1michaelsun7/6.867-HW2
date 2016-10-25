d43d8ab600256dab7ced76c198b45c996f7a73cf
Michael Sun <micsun@mit.edu>
2016-10-25 17:07:47 -0400

    misclassified image code for MNIST

--- hw2_code/mnist.py
+++ hw2_code/mnist.py
@@ -8,9 +8,9 @@ from sklearn import linear_model as sklin
 
 odds = [1,3,5,7,9]
 evens = [2,4,6,8,0]
-pairwise_classifications = [[1,7],[3,5],[4,9], [odds, evens]]
-C = [10**(-i) for i in xrange(6)]
-bands = [0.1,1,4]
+pairwise_classifications = [[odds, evens]]
+C = [0.01, 0.1, 1, 10, 100]
+bands = [10**x for x in range(-1, 2)]
 
 def gaussian_rbf(x1, x2, band):
 	return exp(-linalg.norm(x1-x2)**2/(2.0*band**2))
@@ -49,12 +49,13 @@ def LR_MNIST_pair(pair):
 
 	LR_model = sklin.LogisticRegression()
 	LR_model = LR_model.fit(Xs,Ys)
+	print("Train: ", LR_model.score(Xs, Ys))
 
 	print '======Calculating Validation Accuracy======'
-	neg_val = generate_validation_set(neg) if isinstance(neg, int) else np.array([x for n in neg for x in generate_validation_set(n)])
+	neg_val = generate_test_set(neg) if isinstance(neg, int) else np.array([x for n in neg for x in generate_validation_set(n)])
 	#neg_val = 2*np.true_divide(neg_val, 255) - 1 # normalize
 
-	pos_val = generate_validation_set(pos) if isinstance(pos, int) else np.array([x for p in pos for x in generate_validation_set(p)])
+	pos_val = generate_test_set(pos) if isinstance(pos, int) else np.array([x for p in pos for x in generate_validation_set(p)])
 	#pos_val = 2*np.true_divide(pos_val, 255) - 1 # normalize
 
 	neg_valY = -1*np.ones((neg_val.shape[0],))
@@ -62,6 +63,14 @@ def LR_MNIST_pair(pair):
 
 	X_val = np.concatenate((neg_val, pos_val))
 	Y_val = np.concatenate((neg_valY, pos_valY))
+
+	prediction = LR_model.predict(X_val)
+	same = (prediction == Y_val)
+	misclassified = np.where(same == 0)
+	mis_ex = X_val[misclassified[0][0]]
+	print("Misclassified: ", Y_val[misclassified[0][0]])
+	pl.imshow(mis_ex.reshape((28,28)), cmap=pl.cm.gray)
+	pl.show()
 
 	accuracy = LR_model.score(X_val, Y_val)
 	print("Accuracy: ", accuracy)
@@ -76,10 +85,10 @@ def CSVM_MNIST_pair(pair):
 		pos = pair[1]
 		print '======Training======'
 		neg_training = generate_training_set(neg) if isinstance(neg, int) else np.array([x for n in neg for x in generate_training_set(n)])
-		neg_training = 2*np.true_divide(neg_training, 255) - 1 # normalize
+		#neg_training = 2*np.true_divide(neg_training, 255) - 1 # normalize
 
 		pos_training = generate_training_set(pos) if isinstance(pos, int) else np.array([x for p in pos for x in generate_training_set(p)])
-		pos_training = 2*np.true_divide(pos_training, 255) - 1 # normalize
+		#pos_training = 2*np.true_divide(pos_training, 255) - 1 # normalize
 
 		neg_Y = -1*np.ones((neg_training.shape[0],))
 		pos_Y = np.ones((pos_training.shape[0],))
@@ -91,10 +100,10 @@ def CSVM_MNIST_pair(pair):
 		Ys = np.expand_dims(Ys,1)
 
 		alphas = np.array(qp_sol[0][:])
-		supports = np.where(alphas > 1e-6)
+		supports = np.where(alphas > 1e-8*c)
 		num_supports = len(supports[0])
 
-		marginal = where((1e-6 < alphas) & (alphas < c - 1e-6))
+		marginal = np.where((1e-8*c < alphas) & (alphas < c - 1e-8*c))
 		num_marginal = len(marginal[0])
 
 		def sum_kf(x):
@@ -103,14 +112,14 @@ def CSVM_MNIST_pair(pair):
 		print("Supports: ", num_supports)
 
 		w = np.dot(np.transpose(np.multiply(alphas[supports], Ys[supports])),Xs[supports[0]])
-		b = 1.0/num_marginal*(np.sum(Ys[marginal]-np.apply_along_axis(sum_kf, 1, Xs[marginal[0]])))
+		b = 1.0/num_supports*(np.sum(Ys[supports]-np.apply_along_axis(sum_kf, 1, Xs[marginal[0]])))
 
 		print '======Calculating Validation Accuracy======'
 		neg_val = generate_validation_set(neg) if isinstance(neg, int) else np.array([x for n in neg for x in generate_validation_set(n)])
-		neg_val = 2*np.true_divide(neg_val, 255) - 1 # normalize
+		#neg_val = 2*np.true_divide(neg_val, 255) - 1 # normalize
 
 		pos_val = generate_validation_set(pos) if isinstance(pos, int) else np.array([x for p in pos for x in generate_validation_set(p)])
-		pos_val = 2*np.true_divide(pos_val, 255) - 1 # normalize
+		#pos_val = 2*np.true_divide(pos_val, 255) - 1 # normalize
 
 		neg_valY = -1*np.ones((neg_val.shape[0],))
 		pos_valY = np.ones((pos_val.shape[0],))
@@ -128,16 +137,24 @@ def CSVM_MNIST_pair(pair):
 
 	print '======Testing======'
 	neg_test = generate_test_set(neg) if isinstance(neg, int) else np.array([x for n in neg for x in generate_test_set(n)])
-	neg_test = 2*np.true_divide(neg_test, 255) - 1 # normalize
+	#neg_test = 2*np.true_divide(neg_test, 255) - 1 # normalize
 
 	pos_test = generate_test_set(pos) if isinstance(pos, int) else np.array([x for p in pos for x in generate_test_set(p)])
-	pos_test = 2*np.true_divide(pos_test, 255) - 1 # normalize
+	#pos_test = 2*np.true_divide(pos_test, 255) - 1 # normalize
 
 	neg_testY = -1*np.ones((neg_test.shape[0],))
 	pos_testY = np.ones((pos_test.shape[0],))
 
-	test_results = np.sign(best_b + np.dot(best_w, np.transpose(np.concatenate((neg_test, pos_test)))))
-	accuracy = (test_results == np.concatenate((neg_testY, pos_testY))).sum()/float(test_results.shape[0])
+	test_X = np.concatenate((neg_test, pos_test))
+	test_Y = np.concatenate((neg_testY, pos_testY))
+	test_results = np.sign(best_b + np.dot(best_w, np.transpose(test_X)))
+	same = (test_results == test_Y)
+	misclassified = np.where(same == 0)
+	mis_ex = test_X[misclassified[0][0]]
+	print("Misclassified as: ", test_Y[misclassified[0][0]])
+	pl.imshow(mis_ex.reshape((28,28)), cmap=pl.cm.gray)
+	pl.show()
+	accuracy = same.sum()/float(test_results.shape[0])
 	print("Test Accuracy: ", accuracy)
 
 def RBF_MNIST_pair(pair):
@@ -151,10 +168,10 @@ def RBF_MNIST_pair(pair):
 	pos = pair[1]
 	# Generate training sets
 	neg_training = generate_training_set(neg) if isinstance(neg, int) else np.array([x for n in neg for x in generate_training_set(n)])
-	#neg_training = 2*np.true_divide(neg_training, 255) - 1 # normalize
+	neg_training = 2*np.true_divide(neg_training, 255) - 1 # normalize
 
 	pos_training = generate_training_set(pos) if isinstance(pos, int) else np.array([x for p in pos for x in generate_training_set(p)])
-	#pos_training = 2*np.true_divide(pos_training, 255) - 1 # normalize
+	pos_training = 2*np.true_divide(pos_training, 255) - 1 # normalize
 
 	neg_Y = -1*np.ones((neg_training.shape[0],))
 	pos_Y = np.ones((pos_training.shape[0],))
@@ -164,10 +181,10 @@ def RBF_MNIST_pair(pair):
 
 	# Generate validation sets
 	neg_val = generate_validation_set(neg) if isinstance(neg, int) else np.array([x for n in neg for x in generate_validation_set(n)])
-	#neg_val = 2*np.true_divide(neg_val, 255) - 1 # normalize
+	neg_val = 2*np.true_divide(neg_val, 255) - 1 # normalize
 
 	pos_val = generate_validation_set(pos) if isinstance(pos, int) else np.array([x for p in pos for x in generate_validation_set(p)])
-	#pos_val = 2*np.true_divide(pos_val, 255) - 1 # normalize
+	pos_val = 2*np.true_divide(pos_val, 255) - 1 # normalize
 
 	neg_valY = -1*np.ones((neg_val.shape[0],))
 	pos_valY = np.ones((pos_val.shape[0],))
@@ -233,10 +250,10 @@ def RBF_MNIST_pair(pair):
 
 	print '======Testing======'
 	neg_test = generate_test_set(neg) if isinstance(neg, int) else np.array([x for n in neg for x in generate_test_set(n)])
-	#neg_test = 2*np.true_divide(neg_test, 255) - 1 # normalize
+	neg_test = 2*np.true_divide(neg_test, 255) - 1 # normalize
 
 	pos_test = generate_test_set(pos) if isinstance(pos, int) else np.array([x for p in pos for x in generate_test_set(p)])
-	#pos_test = 2*np.true_divide(pos_test, 255) - 1 # normalize
+	pos_test = 2*np.true_divide(pos_test, 255) - 1 # normalize
 
 	neg_testY = -1*np.ones((neg_test.shape[0],))
 	pos_testY = np.ones((pos_test.shape[0],))
@@ -245,10 +262,52 @@ def RBF_MNIST_pair(pair):
 	test_Y = np.concatenate((neg_testY, pos_testY))
 
 	test_results = np.squeeze(np.apply_along_axis(predictSVM, 1, test_X))
-	accuracy = (test_results == test_Y).sum()/float(test_results.shape[0])
+	same = (test_results == test_Y)
+	misclassified = np.where(same == 0)
+	mis_ex = test_X[misclassified[0][-1]]
+	print("Misclassified as: ", test_Y[misclassified[0][-1]])
+	pl.imshow(mis_ex.reshape((28,28)), cmap=pl.cm.gray)
+	pl.show()
+	accuracy = same.sum()/float(test_results.shape[0])
 	print("Test Accuracy: ", accuracy)
 
+def PEGASOS_MNIST_pair(pair):
+	best_error = 1
+	best_C = -float("inf")
+	best_alphas = None
+	best_width = None
+	best_b = None
+
+	neg = pair[0]
+	pos = pair[1]
+	# Generate training sets
+	neg_training = generate_training_set(neg) if isinstance(neg, int) else np.array([x for n in neg for x in generate_training_set(n)])
+	neg_training = 2*np.true_divide(neg_training, 255) - 1 # normalize
+
+	pos_training = generate_training_set(pos) if isinstance(pos, int) else np.array([x for p in pos for x in generate_training_set(p)])
+	pos_training = 2*np.true_divide(pos_training, 255) - 1 # normalize
+
+	neg_Y = -1*np.ones((neg_training.shape[0],))
+	pos_Y = np.ones((pos_training.shape[0],))
+
+	Xs = np.concatenate((neg_training, pos_training))
+	Ys = np.concatenate((neg_Y,pos_Y))
+
+	# Generate validation sets
+	neg_val = generate_validation_set(neg) if isinstance(neg, int) else np.array([x for n in neg for x in generate_validation_set(n)])
+	neg_val = 2*np.true_divide(neg_val, 255) - 1 # normalize
+
+	pos_val = generate_validation_set(pos) if isinstance(pos, int) else np.array([x for p in pos for x in generate_validation_set(p)])
+	pos_val = 2*np.true_divide(pos_val, 255) - 1 # normalize
+
+	neg_valY = -1*np.ones((neg_val.shape[0],))
+	pos_valY = np.ones((pos_val.shape[0],))
+
+	val_X = np.concatenate((neg_val, pos_val))
+	val_Y = np.concatenate((neg_valY, pos_valY))
+
+
 for pair in pairwise_classifications:
-	#LR_MNIST_pair(pair)
+	LR_MNIST_pair(pair)
 	#CSVM_MNIST_pair(pair)
-	RBF_MNIST_pair(pair)
+	#RBF_MNIST_pair(pair)
